432b55cc37da1b552a619a5138d7ae96085b98bf
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-12-19 17:50:25 +0900

    feat(api): add graphql server
    add graphql-yoga

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,3 +1,21 @@
+import { GraphQLServer } from 'graphql-yoga'
 import message from './message'
 
-console.log(message())
+const typeDefs = `
+  type Query {
+    greeting: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    greeting: message,
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(() => console.log(`Server is running at http://localhost:4000`))
